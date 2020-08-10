b1822e6ea8b40dd739b87ce512d4d9075e43794b
Raj Sree <Rajsree@users.noreply.github.com>
2020-08-10 16:13:03 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (cpp)

--- problems/intersection_of_two_arrays_ii/solution.cpp
+++ problems/intersection_of_two_arrays_ii/solution.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> intersectArray;
-        vector<int> result;
+        unordered_map<int, int> mymap;
+        vector<int> mapArr;
         
         for(auto n1 : nums1) {
-            intersectArray[n1]++; 
+           // cout << n1;
+            mymap[n1]++;
         }
         
         for(auto n2 : nums2) {
-            if(intersectArray[n2] != 0) {
-                intersectArray[n2]--;
-                result.push_back(n2);
+            if(mymap[n2]>=1) {
+                mymap[n2]--;
+                mapArr.push_back(n2);
             }
         }
-        return result;
+               
+        mymap.clear();
+        
+        return mapArr;
     }
 };
