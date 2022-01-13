07d354d576584012f3e3a05d5fbd8be4a1194ed6
Raj Sree <Rajsree@users.noreply.github.com>
2022-01-13 21:58:21 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (javascript)

---
+++ problems/longest_substring_without_repeating_characters/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if(s.length==0) return 0;
+    let substring = "";
+    let max = 1;
+    
+    for(let i=0; i<s.length-1; i++){
+        substring = ""+s[i];
+        
+        for(let j=i+1; j<s.length; j++) {              
+             if(substring.indexOf(s[j])==-1) {
+                 substring+=s[j]; 
+                 
+             } else {
+                  
+                 break; 
+             }                         
+        } 
+        // break;
+         if(substring.length > max) max=substring.length; 
+    }
+    return max;
+    
+};
