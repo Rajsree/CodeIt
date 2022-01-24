c55bc9c75f92e6cffbe5f48c0b33612d9fbec0e6
Raj Sree <Rajsree@users.noreply.github.com>
2022-01-24 03:01:36 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

---
+++ problems/palindrome_number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long int y = 0;
+        int temp = x;
+        if(x<0) return false;
+        while(x) {
+            y = y*10 + x%10;
+             x /= 10;
+        }    
+        if(y==temp) 
+            return true;
+        return false;
+        
+    }
+};
