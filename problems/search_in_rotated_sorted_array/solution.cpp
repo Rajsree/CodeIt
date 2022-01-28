800aaee585b1058eb9834de920f44faf770d6d4e
Raj Sree <Rajsree@users.noreply.github.com>
2022-01-28 22:26:10 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- problems/search_in_rotated_sorted_array/solution.cpp
+++ problems/search_in_rotated_sorted_array/solution.cpp
@@ -2,15 +2,54 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {    
-        int start = 0;
-        int end = nums.size()-1;
-        
-        while(start<=end){
-            if(target==nums[start]) return start;
-            if(target==nums[end]) return end;
-            (target > nums[start]) ?  start++ :  end--;
-            
+    int n= nums.size();
+        if(n==0) return -1;
+        if(n==1) {
+            if(nums[0] == target) return 0;
+            return -1;
         }
-        return -1;
+    
+        int start = 0;
+        int end = n-1;
+        int pivot = -1;
+        int rotation_index = -1;
+        int find_val = -1;
+        
+        if(nums[start] < nums[end]) {
+            rotation_index = 0;
+        }
+        
+        while((start<=end) && (rotation_index<0) ) {         
+             pivot = (end+start)/2;
+            // if(nums[pivot] == target) {return pivot;}
+            if(nums[pivot] > nums[pivot+1]) {
+                rotation_index = pivot+1;
+            } else {
+                if(nums[pivot]>nums[start]) {
+                   start = pivot; 
+                } else {
+                    end = pivot;
+                }                
+            }
+        }
+        
+  
+        if(target==nums[rotation_index]) return rotation_index;
+        if(rotation_index==0) {
+                find_val = lower_bound(nums.begin(), nums.end(),target)-nums.begin();
+        } else {
+            if(target==nums[0]) return 0;
+            
+            if(target>nums[0]) {
+                    find_val = lower_bound(nums.begin(),nums.begin()+rotation_index,target)-nums.begin();
+             } else {
+                    find_val = lower_bound(nums.begin()+rotation_index, nums.end(), target)-nums.begin();
+             }
+        }
+
+        
+        if(find_val!=n && nums[find_val]==target) find_val=find_val;
+        else find_val = -1;
+        return find_val;
     }
 };
