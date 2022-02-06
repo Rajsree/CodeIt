a3db31dfde4048c5231d19e049ba35cb04de8dd8
Raj Sree <Rajsree@users.noreply.github.com>
2022-02-06 13:38:43 +0000

    Sync LeetCode submission - Shortest Path in a Grid with Obstacles Elimination (cpp)

---
+++ problems/shortest_path_in_a_grid_with_obstacles_elimination/solution.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        queue<vector<int>> way;
+        vector<vector<int>> vis(grid.size(), vector<int>(grid[0].size(),-1));
+        
+        int m = grid.size()-1;
+        int n = grid[0].size()-1;  
+        
+        if(m==0 && n==0) return 0;
+        way.push({0,0, 0, k}); //x, y , dist, k obstacles
+        
+        while(!way.empty()) {          
+                auto top = way.front();
+                int x = top[0];
+                int y = top[1];                            
+                way.pop();
+              
+                if(x < 0 || x >= m+1 || y <0 || y >= n+1){
+                    continue;
+                }
+
+            
+
+                if(x==m && y==n) {
+                    return top[2];
+                }
+            
+                if(grid[x][y]==1) {
+                    if(top[3]>0) {
+                        top[3]--;
+                    } else {
+                        continue;
+                    }
+                }
+
+                if(vis[x][y]!=-1 && vis[x][y]>=top[3]) {
+                    continue;
+                }
+                vis[x][y]=top[3];
+
+                way.push({x+1,y,top[2]+1, top[3]});
+                way.push({x-1,y,top[2]+1, top[3]});
+                way.push({x,y+1,top[2]+1, top[3]});
+                way.push({x,y-1,top[2]+1, top[3]});           
+                   
+        }
+        return -1;
+    }
+};
+// pair x, y
+//Shortest Path (0,0)->(m-1,n-1);
+//if(obs) return 1e9;
+//count <= k {
+
+//} else =>return 1e9;
