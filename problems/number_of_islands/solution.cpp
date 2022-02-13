c36ec97c5c7da5b3e7b6a86115810867e0a58117
Raj Sree <Rajsree@users.noreply.github.com>
2022-02-13 01:17:11 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- problems/number_of_islands/solution.cpp
+++ problems/number_of_islands/solution.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        int islands = 0;
+
         int m = grid.size()-1;
         int n= grid[0].size()-1;
-        int islands = 0;
-        for(int i=0; i<=m; i++) {
+        
+        vector<vector<char>> vis(m+2, vector<char>(n+2,'-1'));
+        
+        queue<pair<int,int>> q;
+        for(int i=0; i<=m; i++){
             for(int j=0; j<=n; j++) {
-                if(grid[i][j]=='1'){        
-                    markIslands(grid, i,j, m,n);
+                if(grid[i][j]=='1'){
+                    q.push({i,j});
                     islands++;
+                    
+                while(!q.empty()) {
+                    int k= q.size();
+                    int dx[4] = {1,-1,0,0};
+                    int dy[4] = {0,0,1,-1};
+
+                    while(k--) {
+                        pair<int,int> front = q.front();               
+                        q.pop();
+                        int x = front.first, y = front.second;              
+                        for(int i=0; i<4; i++) {                    
+                            int nx =x+dx[i], ny=y+dy[i];                    
+                            if(nx<0 || nx>m || ny<0 || ny>n || grid[nx][ny]=='0'){
+                                continue;
+                            }                    
+                            grid[nx][ny] = '0';
+                            q.push({nx,ny});                    
+                        }              
+                    }                
+                 }
                 }
             }
-        }       
-        
-        return islands;        
-    }
-    void markIslands(vector<vector<char>> &grid, int i, int j, int m, int n){
-        if(i<0 || j<0 || i>m || j > n || grid[i][j]!='1'){
-            return;
         }
-        grid[i][j] = 2;
-        markIslands(grid, i+1, j , m ,n);
-        markIslands(grid, i-1, j , m ,n);
-        markIslands(grid, i, j+1 , m ,n);
-        markIslands(grid, i, j-1, m ,n);
         
-    }
 
+        
+        // for(int i=0; i<=m; i++) {
+        //     for(int j=0; j<=n; j++) {
+        //         cout << vis[i][j];
+        //     }
+        // }
+        
+        return islands;
+    }
 };
