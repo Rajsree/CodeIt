7b73e072fa0e3b5dcb985c5da845b42988429316
Raj Sree <Rajsree@users.noreply.github.com>
2022-02-14 01:34:38 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (cpp)

--- problems/longest_substring_without_repeating_characters/solution.cpp
+++ problems/longest_substring_without_repeating_characters/solution.cpp
@@ -1,43 +1,28 @@
-    #include<algorithm>
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
-        unordered_set<char> setTry;
-        int N = s.length();
-        if(N==0) return 0;
-        int max = 0;
-        
-        int i=0, j=0;
-        
-        while(i<N && j<N) {
-            
-            if(setTry.find(s[i]) != setTry.end()) {
-                setTry.erase(s[j++]);
-            } else {
-                setTry.insert(s[i++]);
-                max = max > (i-j) ? max : (i-j);
-            }
-            
+        int n = s.length();
+        if(n<=1) {
+            return n;
         }
         
-        return max;
+        map<char, int> entry;
+        int maxlen = 0;
+        int start = 0; 
+        
+        
+        for(int i=0; i<n; i++) {
+            if(entry.find(s[i])!=entry.end()) { 
+                start = max(start, entry[s[i]]+1);            
+            } 
+                maxlen = max(maxlen,i-start+1); 
+                entry[s[i]] = i;                     
+             
+        }
+        
+        
+        return maxlen;
+        
         
     }
 };
-
-// while( i<n && j<n)
-// 		{
-// 			if(set.find(s[j]) == set.end()) //If the character does not in the set
-// 			{
-// 				set.insert(s[j++]); //Insert the character in set and update the j counter
-// 				ans = max(ans, j-i); //Check if the new distance is longer than the current answer
-// 			}
-// 			else
-// 			{
-// 				set.erase(s[i++]); 
-// 				/*If character does exist in the set, ie. it is a repeated character, 
-// 				we update the left side counter i, and continue with the checking for substring. */
-// 			}
-// 		}
