c3499002c64e5b2f4de2e034011cec07bd1dc8a1
Raj Sree <Rajsree@users.noreply.github.com>
2022-02-16 00:10:08 +0000

    Sync LeetCode submission - Single Number (cpp)

--- problems/single_number/solution.cpp
+++ problems/single_number/solution.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        set<int> s1;
-        int n1= nums.size();
-        for(int i=0; i<n1; i++) {
-            if(s1.count(nums[i])) {
-                s1.erase(nums[i]);
-            } else {
-                s1.insert(nums[i]);
-            }
+        int n= nums.size();
+        if(n==1) {
+            return nums[0];
         }
-        set<int> :: iterator itr=s1.begin();
-        return *itr;
+        unordered_set<int> myset;
         
+        for(int i=0; i<n; i++){
+            if(myset.find(nums[i])!=myset.end()) {
+                myset.erase(nums[i]);
+            } else {
+               myset.insert(nums[i]);
+            }
+            
+        }
+        
+        return *myset.begin();
     }
 };
