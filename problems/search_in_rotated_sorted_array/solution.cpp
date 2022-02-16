090bf3f76bbf551371483d59a83d500a885846b0
Raj Sree <Rajsree@users.noreply.github.com>
2022-02-16 17:15:43 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (cpp)

--- problems/search_in_rotated_sorted_array/solution.cpp
+++ problems/search_in_rotated_sorted_array/solution.cpp
@@ -15,13 +15,13 @@ public:
         int rotation_index = -1;
         int find_val = -1;
         
+        //Find the smallest element in rotated array= rotation index
         if(nums[start] < nums[end]) {
             rotation_index = 0;
         }
         
         while((start<=end) && (rotation_index<0) ) {         
-             pivot = (end+start)/2;
-            // if(nums[pivot] == target) {return pivot;}
+             pivot = start + (end-start)/2;
             if(nums[pivot] > nums[pivot+1]) {
                 rotation_index = pivot+1;
             } else {
@@ -38,8 +38,7 @@ public:
         if(rotation_index==0) {
                 find_val = lower_bound(nums.begin(), nums.end(),target)-nums.begin();
         } else {
-            if(target==nums[0]) return 0;
-            
+            if(target==nums[0]) return 0;            
             if(target>nums[0]) {
                     find_val = lower_bound(nums.begin(),nums.begin()+rotation_index,target)-nums.begin();
              } else {
@@ -48,8 +47,7 @@ public:
         }
 
         
-        if(find_val!=n && nums[find_val]==target) find_val=find_val;
-        else find_val = -1;
-        return find_val;
+        if(find_val!=n && nums[find_val]==target) return find_val;
+        return -1;
     }
 };
