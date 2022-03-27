6a105759a157beba52735b2a8b92eb68f291ceec
Raj Sree <Rajsree@users.noreply.github.com>
2022-03-27 17:43:12 -0600

    Time: 98 ms (80.51%), Space: 51.2 MB (9.46%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -5,12 +5,12 @@
 var reverseString = function(s) {
     if(s.length<=1) return s;
     
-    helper(0, s.length-1, s);
+    helper(0, s.length-1);
     
-    function helper(start, end,s) {
+    function helper(start, end) {
         if(start>=end) return;
         [s[start], s[end]] = [s[end],s[start]];
-        helper(start+1, end-1,s);
+        helper(start+1, end-1);
     }
     
 };
