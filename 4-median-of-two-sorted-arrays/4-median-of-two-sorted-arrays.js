73e8985b62f395e1a7d016bda6edc749308a18cb
Raj Sree <Rajsree@users.noreply.github.com>
2022-04-04 04:06:10 -0600

    Time: 185 ms (30.88%), Space: 48.3 MB (37.00%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -7,8 +7,9 @@ var findMedianSortedArrays = function(nums1, nums2) {
     // let merged = [...nums1,...nums2];
     nums1.push(...nums2);
     nums1.sort((a,b) => {return a-b});
+    let mid;
     if(nums1.length%2==0) {
-        let mid = Math.floor(nums1.length/2);
+        mid = Math.floor(nums1.length/2);
         return (nums1[mid-1] + nums1[mid]) / 2;
     } else {
         // console.log(nums1)
