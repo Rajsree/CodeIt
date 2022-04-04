282a47bb9ed986439a7c5666677159fc7a5f2771
Raj Sree <Rajsree@users.noreply.github.com>
2022-04-04 03:50:41 -0600

    Time: 269 ms (26.89%), Space: 48.8 MB (39.26%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -4,23 +4,19 @@
  */
 var lengthOfLongestSubstring = function(s) {
     if(s.length==0) return 0;
-    let substring = "";
     let max = 1;
     
-    for(let i=0; i<s.length-1; i++){
-        substring = ""+s[i];
-        
-        for(let j=i+1; j<s.length; j++) {              
-             if(substring.indexOf(s[j])==-1) {
-                 substring+=s[j]; 
-                 
-             } else {
-                  
-                 break; 
-             }                         
-        } 
-         max = substring.length>max ? substring.length : max; 
+    for(var i=0; i<s.length-1; i++){
+        let substring = ""+s[i];
+        for(var j=i+1; j<s.length; j++) {
+            if(substring.includes(s[j])) {
+                break;
+            } else {
+                substring+=s[j];
+            }
+            max = substring.length>max ? substring.length : max;
+        }
     }
-    return max;
     
+    return max;
 };
