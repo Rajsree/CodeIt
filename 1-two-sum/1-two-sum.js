a67cc1265544d8ef91a1e90125e5096f85d4ef0e
Raj Sree <Rajsree@users.noreply.github.com>
2022-04-04 03:31:39 -0600

    Time: 65 ms (94.09%), Space: 42.8 MB (41.27%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,13 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    if(nums.length == 2) return [0,1];
-    var map = {};
-    for(var i=0;i<nums.length; i++) {
+    let map = {};
+    for(let i=0;i<nums.length; i++) {
         if(target-nums[i] in map) {
             return [i,map[target-nums[i]]];
         }
         map[nums[i]] = i;
     }
-    // return [0,0];
 };
